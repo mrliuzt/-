f56db6a031069c89cb32688cde01bbd5cdeeb403
mrliuzt <3290589369@qq.com>
2026-05-09 23:49:58 +0800

    Fix: always reset password to 123456 if password is 0

--- V1.0/Hardware/Store.c
+++ V1.0/Hardware/Store.c
@@ -4,6 +4,8 @@
 
 uint16_t Store_Data[512];//存储对应一页1024字节
 
+void Store_Save(void);  // 前向声明
+
 void Store_Init(void)
 {
 	if(MyFLASH_ReadHalfWord(0x0800FC00) != 0xA5A5)
@@ -14,15 +16,24 @@ void Store_Init(void)
 		{
 			MyFLASH_ProgramHalfWord(0x800FC00 + i*2,0x0000);
 		}
-		// 设置默认密码为 123456
-		Store_Data[1] = 123456 % 65535;        // 低16位
-		Store_Data[2] = 123456 / 65535;        // 高16位
+		// 第一次设置默认密码为 123456
+		Store_Data[1] = 123456 % 65535;
+		Store_Data[2] = 123456 / 65535;
 		Store_Save();
 	}
-	
+
+	// 从Flash读取数据到SRAM
 	for(uint16_t i = 0;i < 512; i++)
 	{
 		Store_Data[i] = MyFLASH_ReadHalfWord(0x800FC00 + i*2);
+	}
+
+	// 如果密码为0，设置默认密码为123456
+	if(Store_Data[1] == 0 && Store_Data[2] == 0)
+	{
+		Store_Data[1] = 123456 % 65535;
+		Store_Data[2] = 123456 / 65535;
+		Store_Save();
 	}
 }
 
